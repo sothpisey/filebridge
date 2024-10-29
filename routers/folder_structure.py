7de00881cc4edeb12e1a51578d186f3c61e5581c
SOTH_PISEY <60058407+sothpisey@users.noreply.github.com>
2024-10-29 11:13:34 +0000

    add download_link properti into folder_structure api

--- routers/folder_structure.py
+++ routers/folder_structure.py
@@ -10,32 +10,32 @@ with open('config.json', 'r') as file:
     data =  json.load(file)
     
 
-path = Path(data['path'])
+BASE_DIR = Path(data['path'])
 
 class FileStructure(BaseModel):
     name: str
     type: str
+    download_link: str
     children: List[Union["FileStructure", dict]] = []
 
 FileStructure.update_forward_refs()
 
-def create_folder_structure_json(path: Path) -> FileStructure:
-    result = {'name': path.name, 'type': 'folder', 'children': []}
+def create_folder_structure_json(path: Path, BASE_DIR: Path) -> FileStructure:
+    result = {'name': path.name, 'type': 'folder', 'download_link': 'None', 'children': []}
 
     if not path.is_dir():
         return result
 
     for entry in path.iterdir():
         if entry.is_dir():
-            result['children'].append(create_folder_structure_json(entry))
+            result['children'].append(create_folder_structure_json(entry, BASE_DIR))
         else:
-            result['children'].append({'name': entry.name, 'type': 'file'})
+            result['children'].append({'name': entry.name, 'type': 'file', 'download_link': f'/api/download/{entry.relative_to(BASE_DIR.absolute())}'})
 
     return result
 
 
 @router.get("/folder-structure", response_model=FileStructure)
 async def get_folder_structure():
-    folder_path = Path(path)
-    folder_structure = create_folder_structure_json(folder_path)
+    folder_structure = create_folder_structure_json(BASE_DIR, BASE_DIR)
     return folder_structure
