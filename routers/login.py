03bda72aed191af012d73a7cd540c1fd3aa7441a
SOTH_PISEY <60058407+sothpisey@users.noreply.github.com>
2024-11-06 10:14:49 +0000

    add token_generator and verify_token function to login.py file by using random secret key🔑

--- routers/login.py
+++ routers/login.py
@@ -1,6 +1,13 @@
 import hashlib
 import random
 import base64
+import jwt
+from datetime import datetime, timedelta, timezone
+
+
+HS256_SECRET_KEY = base64.b64encode(random.randbytes(64)).decode('utf-8')
+ALGORITHM = 'HS256'
+JWT_TOKEN_EXPIRE_MINUTES = timedelta(minutes=30)
 
 
 def get_password_hash(password: str, salt: bytes = None, iterations: int = None) -> str:
@@ -16,3 +23,22 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     algorithm, iterations, salt, hashed = hashed_password.split('$')
     hashed_from_plain_password = hashlib.pbkdf2_hmac('sha256', plain_password.encode(), base64.b64decode(salt), int(iterations))
     return base64.b64encode(hashed_from_plain_password).decode('utf-8') == hashed
+
+
+def generate_token(payload: dict, hs256_secret_key: str = HS256_SECRET_KEY, expires_delta: timedelta | None = None) -> str:
+    payload = payload.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    payload.update({'exp': int(expire.timestamp())})
+    jwt_token = jwt.encode(payload, hs256_secret_key, algorithm=ALGORITHM)
+    return jwt_token
+
+
+def verify_token(jwt_token: str, hs256_secret_key: str = HS256_SECRET_KEY) -> bool:
+    try:
+        jwt.decode(jwt_token, hs256_secret_key, algorithms=[ALGORITHM])
+        return True
+    except:
+        return False
